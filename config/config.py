ebf5b614e6cae211e1a24c9f10f35a5acf4d1a86
aaitguenissaid <aaitguenissaid@gmail.com>
2025-04-21 13:18:55 +0200

    introduced main config file for paths and constants.

---
+++ config/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+# Constants
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+TRACKING_URI = "http://127.0.0.1:5000"
+MODEL_BASE_NAME = f"prophet-retail-forecaster-store"
+
+# Paths
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+MODEL_DIR = PROJECT_ROOT / "models"
+DATA_DIR = PROJECT_ROOT / "data"
+TRAIN_FILE = DATA_DIR / "train.csv" 
+MLRUNS_DIR = PROJECT_ROOT / "train" / "mlruns"
+RESULTS_DIR = PROJECT_ROOT / "train" / "results"
+
+def get_artifact_path(store_id:int) -> str:
+    return f"model-{store_id:04}"
+
+def get_model_name(store_id : str) -> str:
+    return f"{MODEL_BASE_NAME}-{int(store_id):04}"
+
+def get_model_path( model_dir : str = MODEL_DIR, model_name : str = None, model_version : str = None) -> str:
+    return str(Path(model_dir, model_name, model_version))
+
+def get_model_uri( run_id : str, artifact_path : str) -> str:
+    return str(Path("runs:/", run_id, artifact_path))
